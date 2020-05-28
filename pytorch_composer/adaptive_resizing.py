4b5fa1e05f65a54a87eb3ae03f2b44d54d5cdbf6
E P Dansereau <epierard@live.ca>
2020-05-28 15:30:57 -0400

    extracted args finding func

--- pytorch_composer/adaptive_resizing.py
+++ pytorch_composer/adaptive_resizing.py
@@ -3,13 +3,13 @@ import numpy as np
 import math
 
 
-def dimensions_inference(inpu,oupu):
+def dimensions_inference(inpu, oupu):
     input_features = np.prod(inpu)
     output_fixed = 1
     output_variable_factor = 1
     variables_index = []
     variables = []
-    for i,dim in enumerate(oupu):
+    for i, dim in enumerate(oupu):
         if dim > 0:
             output_fixed *= dim
         elif dim < 0:
@@ -18,26 +18,27 @@ def dimensions_inference(inpu,oupu):
             variables.append(-dim)
     if variables == [] or variables == [-1]:
         return oupu
-    
+
     output_free = input_features / output_fixed
     free_ranks = len(variables_index)
-    n = (output_free / output_variable_factor)**(1/free_ranks)
+    n = (output_free / output_variable_factor)**(1 / free_ranks)
     if not n.is_integer():
         n = max(round(n), 1)
-        resize = (n**free_ranks)*output_variable_factor
+        resize = (n**free_ranks) * output_variable_factor
     new_output = list(oupu)
     variables_sum = sum(variables)
     for i, v in zip(variables_index, variables):
-        new_output[i] = int(v*n)
+        new_output[i] = int(v * n)
     return new_output
+
 
 def collapse_at(list_, ind):
     if len(list_) > ind:
         return list_[:ind] + [np.prod(list_[ind:])]
     return list_
 
-def resize(inpu, oupu):
-    x = rand(inpu)
+
+def resizing_args(inpu, oupu):
     oupu = dimensions_inference(inpu, oupu)
     if np.prod(inpu) != np.prod(oupu):
         for i, (inp, out) in enumerate(zip(inpu, oupu)):
@@ -46,19 +47,37 @@ def resize(inpu, oupu):
                 break
         else:
             first_diff = i + 1
-        reshape_in = collapse_at([1]*max(2 - first_diff,0) + inpu, 4)
-        reshape_out = collapse_at([1]*max(2 - first_diff,0) + oupu, 4)
-        reshape_in = collapse_at(reshape_in, len(reshape_out) -1)
-        reshape_out = collapse_at(reshape_out, len(reshape_in) -1)
+        reshape_in = collapse_at([1] * max(2 - first_diff, 0) + inpu, 4)
+        reshape_out = collapse_at([1] * max(2 - first_diff, 0) + oupu, 4)
+        reshape_in = collapse_at(reshape_in, len(reshape_out) - 1)
+        reshape_out = collapse_at(reshape_out, len(reshape_in) - 1)
         if len(reshape_in) == 3:
             pool = nn.AdaptiveAvgPool1d(reshape_out[2:])
         if len(reshape_in) == 4:
             pool = nn.AdaptiveAvgPool2d(reshape_out[2:])
         if len(reshape_in) == 5:
             pool = nn.AdaptiveAvgPool3d(reshape_out[2:])
-        x = x.view(reshape_in)
+        return [reshape_in, reshape_out[2:], oupu]
+    else:
+        return [oupu]
+
+
+def resize(x, oupu):
+    oupu = dimensions_inference(list(x.shape), oupu)
+    args = resizing_args(list(x.shape), oupu)
+    if len(args) == 3:
+        if len(args[0]) == 3:
+            pool = nn.AdaptiveAvgPool1d(args[1])
+        if len(args[0]) == 4:
+            pool = nn.AdaptiveAvgPool2d(args[1])
+        if len(args[0]) == 5:
+            pool = nn.AdaptiveAvgPool3d(args[1])
+        x = x.view(args[0])
         x = pool(x)
     x = x.view(oupu)
-    return x.shape
+    return x
 
-resize(inpu = [4,63], oupu = [4,1,-1,-1])
+
+x = rand([4, 63])
+x = resize(x, oupu=[4, 1, -1, -1])
+x.shape
