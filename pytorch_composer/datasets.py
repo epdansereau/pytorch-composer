71ff0951ad0148088e0969322982d17ce6a38c6a
E P Dansereau <epierard@live.ca>
2020-06-15 14:38:48 -0400

    added MNIST

--- pytorch_composer/datasets.py
+++ pytorch_composer/datasets.py
@@ -42,4 +42,44 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=${batch_size},
         self.variables.add_variable("x",[self["batch_size"],3,32,32],0)
         self.variables.add_variable("y",[self["batch_size"]],0,10)
         
+class MNIST(CodeSection):
+    def __init__(self, settings = None):
+        template = '''
+# Load and normalize the dataset
+transform = transforms.Compose(
+    [transforms.ToTensor()])
+
+trainset = torchvision.datasets.MNIST(root='${root}', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=${batch_size},
+                                          shuffle=True, num_workers=${num_workers})
+
+testset = torchvision.datasets.MNIST(root='${root}', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=${batch_size},
+                                         shuffle=False, num_workers=${num_workers})
+'''
+        defaults = {
+            "batch_size":4,
+            "num_workers":2,
+            "root":"./data"
+        }
+        
+        imports = set((
+            "torch",
+            "torchvision",
+            "torchvision.transforms as transforms"
+        ))
+        super().__init__(None, settings, defaults, template, imports)
+        
+    def setitem(self, key, item):
+        self.entered_settings[key] = item
+        if key == "batch_size":
+            self.set_variables(None)
+    
+    def set_variables(self, _):
+        super().set_variables(None)
+        self.variables.add_variable("x",[self["batch_size"],1,28,28],0)
+        self.variables.add_variable("y",[self["batch_size"]],0,10)
+        
 
